6d567df96fd561dcdde0699543c7e1dccd3e9a4d
jonathan dannel <usixjad@gmail.com>
2020-03-21 05:40:53 -0400

    add more solutions for count_smaller

--- count_smaller.py
+++ count_smaller.py
@@ -4,10 +4,32 @@
 # Output: [4,0,1,1,3]
 
 def smaller_than_current(nums):
-	length = len(nums)
-	output = [0] * length
-	for i in range(length):
-		for j in range(length):
+	output = [0] * len(nums)
+	for i in range(len(nums)):
+		for j in range(len(nums)):
 			if nums[j] < nums[i]:
 				output[i] += 1
 	return output
+
+def smaller_than_c(nums):
+	hash = {}
+	for i in range(len(nums)):
+		hash[i] = 0
+		for j in range(len(nums)):
+			if nums[j] < nums[i]:
+				hash[i] += 1
+	return hash.values()
+
+def smaller_than_curr(nums):
+	result = [0] * len(nums)
+	hash = {}
+	for i in range(len(nums)):
+		if nums[i] in hash:
+			result[i] = hash[nums[i]]
+			continue
+		for j in range(len(nums)):
+			if i != j:
+				if nums[i] > nums[j]:
+					result[i] += 1
+		hash[nums[i]] = result[i]
+	return result
